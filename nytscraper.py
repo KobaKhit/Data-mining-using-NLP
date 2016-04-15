68a5875e2b4dd52b51710d96ff6a11eedbd2e3c2
Kobakhit <kobakhit@gmail.com>
2016-04-15 19:14:11 -0400

    added py3.5 and json save

--- nytscraper.py
+++ nytscraper.py
@@ -3,77 +3,97 @@ from urllib2 import urlopen
 from urllib2 import Request
 from pprint import pprint
 # import pandas as pd
+import json
 
 from bs4 import BeautifulSoup
 
 # To do:
 #   - articles have multiple pages, so scrape all to get full text 
-#   - save data as csv
-#   - remove duplicate urls from the list
+
+def scrape(urllist):
+    # Scrape new york time articles given a list of urls
+    # - input:
+    #   list of urls
+    # - output:
+    #   list of objects
+
+    dump = []
+    count = 1
+    for URL in urllist:
+        try:
+            print str(count) + "/" + str(len(urllist)), URL
+
+            r=urlopen(URL).read()
+            soup = BeautifulSoup(r,"html.parser")
+
+            # print (soup.prettify())
+
+            title = soup.find("meta", { "name" : "hdl" })['content']
+            description = soup.find("meta", { "name" : "description" })['content']
+            author = soup.find("meta", { "name" : "byl" })['content'].replace("By ","")
+            keywords = soup.find("meta", { "name" : "keywords" })['content']
+
+            datePublished = soup.find("meta", { "itemprop" : "datePublished" })['content']
+            dateModified = soup.find("meta", { "itemprop" : "dateModified" })['content']
+            dateDisplay = soup.find("meta", { "name" : "DISPLAYDATE" })['content']
+            
+            nextPage = soup.find('a',{'title': 'Next Page'})
+
+            body = soup.find("div", { "class" : "articleBody" }).find_all('p',{'itemprop': 'articleBody'})
+            body = '\n'.join([w.get_text() for w in body])
+
+            # if(nextPage):
+
+            # append to dump list
+            dump.append(
+                {"title": title, 
+                "description": description, 
+                "author": author, 
+                "keywords": keywords, 
+                "datePublished": datePublished, 
+                "dateModified": dateModified,
+                "dateDisplay": dateDisplay, 
+                "body": body})
+
+            count += 1
+        except:
+            print("this article is private")
+            count += 1
+            pass
+
+    return(dump)
 
 def main():
-    NPAGES = 2 # pages to scrape for article urls
-    NARTICLE = 10 # number of articles to download
+    NPAGES = 1 # pages to scrape for article urls
 
     # get article urls
     weburl = []
     for i in range(0,NPAGES):
-        response = urlopen('http://query.nytimes.com/svc/add/v1/sitesearch.json?q=corporate%20espionage&page='+str(i)+'&facet=true', 'JSONOne.json')
-        with open('JSONOne.json') as data_file:    
-            data = json.load(data_file)
-        
-        for j in range(0,NARTICLE):
-            weburl.append(data["response"]["docs"][j]["web_url"])
-            
-    # pprint(weburl)
+        response = urlopen('http://query.nytimes.com/svc/add/v1/sitesearch.json?q=corporate%20espionage&page='+str(i)+'&facet=true')
+        data = json.load(response)
 
-    # dump urls into a txt
-    with open('URList.txt', mode='wt') as myfile:
-        myfile.write('\n'.join(weburl))
+        # print(len(data["response"]["docs"]))
+
+        for j in range(0,len(data["response"]["docs"])):
+            print(data["response"]["docs"][j]["web_url"])
+            weburl.append(data["response"]["docs"][j]["web_url"])
+        
 
     #Cleaning url the list
-    weburl1= []
-    weburl1 = [s for s in weburl if "http://www.nytimes.com" in s]
-    print("Articles to scape: ",len(weburl1))
+    weburl  = [s for s in weburl if "http://www.nytimes.com" in s]
+    print("Articles to scape: ",len(weburl),"\n")
 
     # Parse using BeautifulSoup
     # scrape nyt for articles
-    dump = []
-    for URL in weburl1:
-        print str(weburl1.index(URL)+1) + "/" + str(len(weburl1)), URL
+    dump = scrape(weburl)
 
-        r=urlopen(URL).read()
-        soup = BeautifulSoup(r,"html.parser")
+     # save as json
+    datajson = json.dumps({"data": dump})
+    with open("data.json", "w") as jsonfile:
+        jsonfile.write(datajson)
 
-        # print (soup.prettify())
-
-        title = soup.find("meta", { "name" : "hdl" })['content']
-        description = soup.find("meta", { "name" : "description" })['content']
-        author = soup.find("meta", { "name" : "byl" })['content'].replace("By ","")
-        keywords = soup.find("meta", { "name" : "keywords" })['content']
-
-        datePublished = soup.find("meta", { "itemprop" : "datePublished" })['content']
-        dateModified = soup.find("meta", { "itemprop" : "dateModified" })['content']
-        dateDisplay = soup.find("meta", { "name" : "DISPLAYDATE" })['content']
-        
-        nextPage = soup.find('a',{'title': 'Next Page'})
-
-        body = soup.find("div", { "class" : "articleBody" }).find_all('p',{'itemprop': 'articleBody'})
-        body = '\n'.join([w.get_text() for w in body])
-
-        # if(nextPage):
-
-        # append to dump list
-        dump.append([title, description, author, keywords, datePublished, dateModified, dateDisplay, body])
-
-    # join text elemets with lines as separators
-    dump = [("\n"+"-"*10+"\n").join(w).encode('utf-8').strip() for w in dump]
-
-    # save as a txt
-    with open('Dump.txt', mode='wt') as myfile:
-        myfile.write(("\n\n"+"*"*20+"\n\n").join(dump))
-
-    print "\nDone"
+    print("\nArticles scraped: ", len(dump))
+    print("\nDone")
 
 if __name__ == "__main__":
     main()
